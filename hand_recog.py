486073bcfbc2390d13191a393a3973d0c3045861
Adam <adam.przybyszewski@student.put.poznan.pl>
2017-03-31 16:16:08 +0200

    Changed all variables, functions and class name to english

--- hand_recog.py
+++ hand_recog.py
@@ -3,118 +3,118 @@ import numpy
 import cv2 as opencv
 
 #klasa zbierajaca cala logike przetwarzania obrazu i obliczania charakterystyki dloni
-class RozpoznawanieDloni(object):
-    def __init__(self, sciezka_bazowa, pliki_dloni, plik_wynikowy):
-        self.pliki_dloni = pliki_dloni
-        self.plik_wynikowy = plik_wynikowy
-        self.sciezka_bazowa = sciezka_bazowa
+class HandRecognition(object):
+    def __init__(self, base_path, hand_files, result_file):
+        self.hand_files = hand_files
+        self.result_file = result_file
+        self.base_path = base_path
 
     #wykonywanie obliczen koncowych
-    def wykonaj_obliczenia(self):
-        kontury = []
-        punkty = []
-        dlugosci = []
-        stosunek = []
-        with open(plik_wynikowy, "w") as plk_wyn:
-            for i in range(0,len(pliki_dloni)):
-                sciezka = self.sciezka_bazowa + pliki_dloni[i] 
-                obraz = opencv.imread(sciezka) 
-                kontury.append(self.wykryj_kontur(obraz))
-                punkty.append(self.wykryj_punkty_skrajne(kontury[i]))
-                dlugosci.append(self.oblicz_dlugosci(punkty[i]))
+    def do_calculations(self):
+        contours = []
+        points = []
+        lenghts = []
+        ratio = []
+        with open(result_file, "w") as res_file:
+            for i in range(0,len(hand_files)):
+                path = self.base_path + hand_files[i]
+                picture = opencv.imread(path)
+                contours.append(self.detect_contour(picture))
+                points.append(self.detect_extreme_points(contours[i]))
+                lenghts.append(self.count_lenghts(points[i]))
                 if i > 0:
-                    stosunek.append(self.porownaj_dlugosci(dlugosci[0], dlugosci[i]))
-                print pliki_dloni[i], ' ', '%s\n' % dlugosci[i]
+                    ratio.append(self.compare_lenghts(lenghts[0], lenghts[i]))
+                print(hand_files[i], ' ', '%s\n' % lenghts[i])
             
             #plk_wyn.write('Podobienstwo pliku %s\n' % pliki_dloni[0])
-            for i in range(0, len(stosunek)):
-                plk_wyn.write('Plik %s jest podobny do pliku %s w %s%%\n' % (pliki_dloni[0], pliki_dloni[i+1], stosunek[i]))
+            for i in range(0, len(ratio)):
+                res_file.write('File %s is similar to %s in %s%%\n' % (hand_files[0], hand_files[i+1], ratio[i]))
 
-            obraz1 = opencv.imread(self.sciezka_bazowa + pliki_dloni[0])
-            self.rysuj_kontur(kontury[0], obraz1)
-            self.rysuj_linie(obraz1, punkty)
+            picture1 = opencv.imread(self.base_path + hand_files[0])
+            self.draw_contour(contours[0], picture1)
+            self.draw_lines(picture1, points)
 
-            opencv.imshow("Obraz z konturem", obraz1)
+            opencv.imshow("Picture with contour", picture1)
 
 
     #wykrywanie konturu dloni
-    def wykryj_kontur(self, obraz):
-        ycrcb_obraz = opencv.cvtColor(obraz, opencv.COLOR_BGR2YCR_CB)
-        ycrcb_min_skora = numpy.array((0, 145, 80)) #wartosci sprawdzone w internecie i skorygowane, aby pasowaly do wykonanych zdjec
-        ycrcb_max_skora = numpy.array((255, 170, 130)) #jak wyzej^
-        ycrcb_skora = opencv.inRange(ycrcb_obraz, ycrcb_min_skora, ycrcb_max_skora)
-        _, kontury, _ = opencv.findContours(ycrcb_skora, opencv.RETR_EXTERNAL, opencv.CHAIN_APPROX_SIMPLE)
+    def detect_contour(self, picture):
+        ycrcb_picture = opencv.cvtColor(picture, opencv.COLOR_BGR2YCR_CB)
+        ycrcb_min_skin = numpy.array((0, 145, 80)) #wartosci sprawdzone w internecie i skorygowane, aby pasowaly do wykonanych zdjec
+        ycrcb_max_skin = numpy.array((255, 170, 130)) #jak wyzej^
+        ycrcb_skin = opencv.inRange(ycrcb_picture, ycrcb_min_skin, ycrcb_max_skin)
+        _, contours, _ = opencv.findContours(ycrcb_skin, opencv.RETR_EXTERNAL, opencv.CHAIN_APPROX_SIMPLE)
         
-        return kontury
+        return contours
 
     #rysowanie konturu na zdjeciu
-    def rysuj_kontur(self, kontury, obraz):
-        for i, c in enumerate(kontury):
-            powierzchnia = opencv.contourArea(c)
-            if powierzchnia > 1000:
-                opencv.drawContours(obraz, kontury, i, (255, 0, 0), 5)
+    def draw_contour(self, contours, picture):
+        for i, c in enumerate(contours):
+            area = opencv.contourArea(c)
+            if area > 1000:
+                opencv.drawContours(picture, contours, i, (255, 0, 0), 5)
 
     #rysowanie linii laczacych punkty skrajne
-    def rysuj_linie(self, obraz, punkty):
-        for i in range(1, len(punkty[0])):
-            opencv.line(obraz, tuple(punkty[0][i-1]), tuple(punkty[0][i]), (0, 0, 255), 4)
+    def draw_lines(self, picture, points):
+        for i in range(1, len(points[0])):
+            opencv.line(picture, tuple(points[0][i - 1]), tuple(points[0][i]), (0, 0, 255), 4)
                 
          
     #wykrywanie punktow skrajnych na rece
-    def wykryj_punkty_skrajne(self, kontury):     
-        punkty = []
-        kierunek = {'gdzie': 'gora'}
+    def detect_extreme_points(self, contours):     
+        points = []
+        direction = {'where': 'up'}
 
-        def przelicz_kontur(kontur, kierunek):
-            for i, punkt in enumerate(kontur):
-                x,y = punkt
-                if i < len(kontur) - 3:
-                    x1, y1 = kontur[i + 1]
-                    x2, y2 = kontur[i + 2]
-                    if y > max(y1, y2) and kierunek['gdzie'] == 'gora':
-                        punkty.append([x,y])
-                        kierunek['gdzie'] = 'dol'
-                    elif y < min(y1, y2) and kierunek['gdzie'] == 'dol':
-                        punkty.append([x,y])
-                        kierunek['gdzie'] = 'gora'
+        def count_contour(contour, direction):
+            for i, point in enumerate(contour):
+                x,y = point
+                if i < len(contour) - 3:
+                    x1, y1 = contour[i + 1]
+                    x2, y2 = contour[i + 2]
+                    if y > max(y1, y2) and direction['where'] == 'up':
+                        points.append([x,y])
+                        direction['where'] = 'down'
+                    elif y < min(y1, y2) and direction['where'] == 'down':
+                        points.append([x,y])
+                        direction['where'] = 'up'
         
-        [przelicz_kontur(kontur.reshape(-1, 2), kierunek) for kontur in kontury]
+        [count_contour(contour.reshape(-1, 2), direction) for contour in contours]
                         
-        return punkty
+        return points
 
                     
     #obliczanie proporcji
-    def oblicz_dlugosci(self, punkty):
-        dlugosci = []
-        for wart, wart_nast in zip(punkty, punkty[1:]+[punkty[0]]):
-            dlugosci.append(sqrt(pow(wart_nast[0]-wart[0], 2) + pow(wart_nast[1]-wart[1], 2)))
+    def count_lenghts(self, points):
+        lenghts = []
+        for value, next_value in zip(points, points[1:]+[points[0]]):
+            lenghts.append(sqrt(pow(next_value[0]-value[0], 2) + pow(next_value[1]-value[1], 2)))
             
-        return dlugosci
+        return lenghts
 
                 
     #porownanie konturow
-    def porownaj_dlugosci(self, dlugosci1, dlugosci2):
-        stosunek = 1;
+    def compare_lenghts(self, lenghts1, lenghts2):
+        ratio = 1;
         for i in range(0, 8):
-            if dlugosci1[i] >= dlugosci2[i]:
-                stosunek += dlugosci2[i]/dlugosci1[i]
+            if lenghts1[i] >= lenghts2[i]:
+                ratio += lenghts2[i] / lenghts1[i]
             else:
-                stosunek += dlugosci1[i]/dlugosci2[i]
+                ratio += lenghts1[i] / lenghts2[i]
                 
-        return int(stosunek * 100 / 9)
+        return int(ratio * 100 / 9)
 
 
 #main
 if __name__ == "__main__":
-    pliki_dloni = ['adam1.png', 'adam2.png', 'adam3.png', 'pawel1.png', 'pawel2.png', 'pawel3.png',
+    hand_files = ['adam1.png', 'adam2.png', 'adam3.png', 'pawel1.png', 'pawel2.png', 'pawel3.png',
                    'maciej1.png', 'maciej2.png', 'norbi1.png', 'norbi2.png', 'norbi3.png', 'oli1.png', 'oli2.png', 'oli3.png'] 
 
-    plik_wynikowy = "D:\\PythonProjects\\rozpoznawanie_dloni\\wyniki.txt"
-    sciezka_bazowa = "D:\\PythonProjects\\rozpoznawanie_dloni\\rece\\"
+    result_file = "D:\\PythonProjects\\hand_recognition\\results.txt"
+    base_path = "D:\\PythonProjects\\hand_recognition\\hands"
     
-    rozpoznawacz = RozpoznawanieDloni(sciezka_bazowa, pliki_dloni, plik_wynikowy)
+    recognizer = HandRecognition(base_path, hand_files, result_file)
 
-    rozpoznawacz.wykonaj_obliczenia()
+    recognizer.do_calculations()
     
     opencv.waitKey(0)
     opencv.destroyAllWindows()
